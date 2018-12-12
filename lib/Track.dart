cf1be83ba618d93123886ad4ccf0e82d3ca70969
Leonid Emelaynov <Emelyanov.L.Dm@sberbank.ru>
2018-12-12 14:27:14 +0300

    Code reformat

---
+++ lib/Track.dart
@@ -0,0 +1,7 @@
+class Track {
+  final String name;
+  final String artist;
+  final String imgUrl;
+
+  Track({this.name, this.artist, this.imgUrl});
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:lol_kek/Loader.dart';
+import 'package:lol_kek/Track.dart';
 
 void main() => runApp(MyApp());
 
@@ -86,31 +86,4 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-}
-
-class Loader {
-  final _key = 'd64be117563ee910b260f172319b3001';
-
-  Future<List<Track>> getTopTracks() async {
-    final url =
-        "http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=$_key&format=json";
-    final tracks = List<Track>();
-    final responce = await http.get(url);
-
-    json.decode(responce.body)['tracks']['track'].forEach((track) {
-      tracks.add(Track(
-          name: track['name'],
-          artist: track['artist']['name'],
-          imgUrl: track['image'][3]['#text']));
-    });
-    return tracks;
-  }
-}
-
-class Track {
-  final String name;
-  final String artist;
-  final String imgUrl;
-
-  Track({this.name, this.artist, this.imgUrl});
 }
